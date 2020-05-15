921e336da18256baacc605fdfcd1f3b0f741794b
Phillip Wheatley <phillip.wheatley@skyscanner.net>
2020-05-15 10:31:46 +0100

    No need to rename columns, simply reordering is enough

--- _lackey/_scripts/convert_tsv.rb
+++ _lackey/_scripts/convert_tsv.rb
@@ -5,20 +5,7 @@ original = CSV.parse(csv_text, {:col_sep => "\t", :quote_char => "\x00"})
 
 CSV.open(ARGV[1], "wb", {:col_sep => "\t", :quote_char => "\x00"}) do |csv|
   indices = [1, 2, 0] + (3 ... original.to_a[0].length).to_a
-  header_row = true
-  columns = ["id", "name", "set"].to_a
   original.to_a.each do |row|
-    csv << indices.map { |index|
-      if header_row
-        if index < columns.length
-          columns[index]
-        else
-          row[index]
-        end
-      else
-        row[index]
-      end
-    }
-    header_row = false
+    csv << indices.map { |index| row[index] }
   end
 end
