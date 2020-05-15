c2dc955b0b5ce95a9518ac0fdb5e18cdab85d304
Phillip Wheatley <phillip.wheatley@skyscanner.net>
2020-05-15 10:09:32 +0100

    Test column names

--- _lackey/_scripts/convert_tsv.rb
+++ _lackey/_scripts/convert_tsv.rb
@@ -6,7 +6,7 @@ original = CSV.parse(csv_text, {:col_sep => "\t", :quote_char => "\x00"})
 CSV.open(ARGV[1], "wb", {:col_sep => "\t", :quote_char => "\x00"}) do |csv|
   indices = [1, 2, 0] + (3 ... original.to_a[0].length).to_a
   header_row = true
-  columns = ["ImageFile", "Name", "Set"].to_a
+  columns = ["id", "name", "set"].to_a
   original.to_a.each do |row|
     csv << indices.map { |index|
       if header_row
